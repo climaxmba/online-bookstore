f5979634ca3f2f2c00c6ac8532c31f04717502b3
Climax Mba <climaxium150@gmail.com>
2024-08-11 15:33:40 +0100

    Add constants

---
+++ src/_lib/constants.ts
@@ -0,0 +1,10 @@
+export const paths = {
+  root: "/",
+  deals: "/deals",
+  books: "/books",
+  details: "/books/:packageId",
+  wishlist: "/wishlist",
+  search: "/search",
+  cart: "/cart",
+  checkout: "/checkout",
+};
