c51414d63da1817cd81337f95f9b06a8822ddd44
Climax Mba <climaxium150@gmail.com>
2024-08-11 23:17:10 +0100

    Add categories & trending

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,12 @@
 import Carousel from "react-multi-carousel";
 import Layout from "../../components/layout/Layout";
 import SearchBox from "../../components/searchBox/SearchBox";
+import {
+  CasesTwoTone,
+  HistoryEduTwoTone,
+  LandscapeTwoTone,
+  MenuBookTwoTone,
+} from "@mui/icons-material";
 import { carouselResponsive } from "../../_lib/constants";
 
 import "react-multi-carousel/lib/styles.css";
@@ -11,6 +17,8 @@ export default function Home() {
     <Layout>
       <div className={styles.container}>
         <HeroSection />
+        <CategorySection />
+        <TrendingBooks />
       </div>
     </Layout>
   );
@@ -20,7 +28,7 @@ function HeroSection() {
   return (
     <div className={styles.heroSection}>
       <h1>Featured Books</h1>
-      
+
       <Carousel
         autoPlay
         infinite
@@ -35,6 +43,53 @@ function HeroSection() {
       </Carousel>
 
       <SearchBox />
+    </div>
+  );
+}
+
+function CategorySection() {
+  return (
+    <div className={styles.categorySection}>
+      <h1>Browse by Category</h1>
+      <div className={styles.categories}>
+        <div>
+          <MenuBookTwoTone sx={{ fontSize: 120 }} />
+          Fiction
+        </div>
+        <div>
+          <LandscapeTwoTone sx={{ fontSize: 120 }} />
+          Adventure
+        </div>
+        <div>
+          <CasesTwoTone sx={{ fontSize: 120 }} />
+          Mystery
+        </div>
+        <div>
+          <HistoryEduTwoTone sx={{ fontSize: 120 }} />
+          History
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function TrendingBooks() {
+  return (
+    <div className={styles.trendingBooks}>
+      <h1>Trending Books</h1>
+
+      <Carousel
+        autoPlay
+        infinite
+        autoPlaySpeed={6000}
+        responsive={carouselResponsive}
+      >
+        <div className={styles.bookItem}></div>
+        <div className={styles.bookItem}></div>
+        <div className={styles.bookItem}></div>
+        <div className={styles.bookItem}></div>
+        <div className={styles.bookItem}></div>
+      </Carousel>
     </div>
   );
 }
