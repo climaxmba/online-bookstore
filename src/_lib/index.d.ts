eef18012c065814351035300626e47cf01375570
Climax Mba <climaxium150@gmail.com>
2024-08-11 15:33:23 +0100

    Write declaration and _redirects

---
+++ src/_lib/index.d.ts
@@ -0,0 +1,19 @@
+type Category = "Fiction" | "Adventure" | "Mystery" | "History"
+
+interface Review {
+  author: string;
+  message: string;
+}
+
+interface Book {
+  id: string;
+  title: string;
+  rating: number;
+  reviews: Review[];
+  author: string;
+  price: number;
+  category: Category;
+  isTrending: boolean;
+  image: string;
+  discount: number;
+}
