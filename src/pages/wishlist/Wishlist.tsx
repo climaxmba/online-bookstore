465b452c1c06f83a2e63a58b31369774efe31171
Climax Mba <climaxium150@gmail.com>
2024-08-14 20:33:06 +0100

    Add wishlist page

--- src/pages/wishlist/Wishlist.tsx
+++ src/pages/wishlist/Wishlist.tsx
@@ -1,7 +1,41 @@
+import { useSelector } from "react-redux";
+
 import Layout from "../../components/layout/Layout";
+import BookItem from "../../components/bookItem/BookItem";
+import { RootState } from "../../_lib/redux/store";
 
-// import styles from "./wishlist.module.scss";
+import styles from "./wishlist.module.scss";
 
-export default function Wishlist() {
-  return <Layout>Wishlist</Layout>;
+export default function Category() {
+  const wishlist = useSelector((state: RootState) => state.wishlist.value);
+
+  return (
+    <Layout>
+      <h1
+        style={{
+          marginTop: "5rem",
+          marginLeft: "1rem"
+        }}
+      >
+        Wishlist
+      </h1>
+      {wishlist.length ? (
+        <section className={styles.container}>
+          {wishlist.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      ) : (
+        <p
+          style={{
+            textAlign: "center",
+            fontSize: "large",
+            fontStyle: "italic",
+          }}
+        >
+          Oops! Noting's here yet.
+        </p>
+      )}
+    </Layout>
+  );
 }
