b6a77c02170a7700ed0e303c131346580917177c
Climax Mba <climaxium150@gmail.com>
2024-08-14 10:24:22 +0100

    Modify hero section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,19 @@
+import { SetStateAction, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Carousel from "react-multi-carousel";
-import Layout from "../../components/layout/Layout";
-import SearchBox from "../../components/searchBox/SearchBox";
+
 import {
   CasesOutlined,
   HistoryEduOutlined,
   LandscapeOutlined,
   MenuBookOutlined,
 } from "@mui/icons-material";
+import Layout from "../../components/layout/Layout";
+import SearchBox from "../../components/searchBox/SearchBox";
+import { Spinner, SpinnerError } from "../../components/loading/loading";
+import BookItem from "../../components/bookItem/BookItem";
 import { carouselResponsive, paths } from "../../_lib/constants";
+import booksAPI from "../../_lib/modules/booksAPI";
 
 import "react-multi-carousel/lib/styles.css";
 import styles from "./home.module.scss";
@@ -26,25 +31,71 @@ export default function Home() {
 }
 
 function HeroSection() {
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState<Book[] | []>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setBooks(
+          (await booksAPI.getFeaturedBooks()) as SetStateAction<Book[] | []>
+        );
+      } catch {
+        setError(true);
+      }
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <div className={styles.heroSection}>
-      <h1>Featured Books</h1>
+      <div className={styles.pannels}>
+        <div>
+          <h1>Online Bookstore</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi
+            molestiae error enim accusantium vel similique.
+          </p>
+        </div>
 
-      <Carousel
-        autoPlay
-        rewind
-        rewindWithAnimation
-        autoPlaySpeed={6000}
-        responsive={carouselResponsive}
-      >
-        <div className={styles.bookItem}></div>
-        <div className={styles.bookItem}></div>
-        <div className={styles.bookItem}></div>
-        <div className={styles.bookItem}></div>
-        <div className={styles.bookItem}></div>
-      </Carousel>
+        <SearchBox />
+      </div>
 
-      <SearchBox />
+      <h2>Featured Books</h2>
+
+      {loading ? (
+        <Spinner />
+      ) : error ? (
+        <SpinnerError />
+      ) : books.length ? (
+        <Carousel
+          autoPlay
+          rewind
+          rewindWithAnimation
+          autoPlaySpeed={6000}
+          responsive={carouselResponsive}
+          className={styles.carousel}
+        >
+          {books.map((book) => (
+            <BookItem
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              price={book.price}
+              image={book.image}
+              rating={book.rating}
+              reviews={book.reviews}
+              author={book.author}
+              category={book.category}
+              isTrending={book.isTrending}
+              discount={book.discount}
+            />
+          ))}
+        </Carousel>
+      ) : (
+        <p>No results!</p>
+      )}
     </div>
   );
 }
