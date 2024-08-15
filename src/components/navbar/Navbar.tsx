43a2e5ff069ee6f4cc7e82b146cca009360a00ac
Climax Mba <climaxium150@gmail.com>
2024-08-15 05:40:10 +0100

    Add mobile navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from "react-router-dom";
 import { paths } from "../../_lib/constants";
 
 import styles from "./navbar.module.scss";
+import { useState } from "react";
 
 export default function Navbar() {
   return (
@@ -72,10 +73,94 @@ export default function Navbar() {
           </li>
         </ul>
       </nav>
+
+      <MobileNav />
     </>
   );
 }
 
 function Logo() {
   return <Link to={paths.root}>Online Bookstore</Link>;
+}
+
+function MobileNav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleMenuClick = () => setMenuOpen((val) => !val);
+
+  return (
+    <nav className={styles.mobileContainer}>
+      <Logo />
+      <button
+        title="Menu"
+        className={`${styles.menu} ${menuOpen && styles.menuOpen}`}
+        onClick={handleMenuClick}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <ul className={`${styles.links} ${menuOpen && styles.linksOpen}`}>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.link
+            }
+            to={paths.root}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.link
+            }
+            to={paths.books}
+          >
+            Books
+          </NavLink>
+        </li>
+        <li>
+          <a className={styles.link} href="/#categories">
+            Categories
+          </a>
+        </li>
+        <li>
+          <a className={styles.link} href="/#trending">
+            Trending
+          </a>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.link
+            }
+            to={paths.deals}
+          >
+            Deals
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.link
+            }
+            to={paths.wishlist}
+          >
+            Wishlist
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? styles.active : styles.link
+            }
+            to={paths.cart}
+          >
+            Cart
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
