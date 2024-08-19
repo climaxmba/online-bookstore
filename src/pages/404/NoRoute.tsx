4ce9cecea1f2bd7f1433d4751e24cabc2bea2010
Climax Mba <climaxium150@gmail.com>
2024-08-19 11:14:35 +0100

    Modify 404 page

--- src/pages/404/NoRoute.tsx
+++ src/pages/404/NoRoute.tsx
@@ -1,5 +1,19 @@
-// import styles from "./fourZeroFour.module.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logoColored.svg"
+import styles from "./noRoute.module.scss";
 
 export default function NoRoute() {
-  return <div>404</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <img src={logo} alt="Logo" height={40}/>
+        <h1>Online Bookstore</h1>
+      </div>
+      <p className={styles.errorCode}>404</p>
+      <h2>Oh no, this route doesn't exist!</h2>
+      <Link to="/">
+        Visit Home page
+      </Link>
+    </div>
+  );
 }
