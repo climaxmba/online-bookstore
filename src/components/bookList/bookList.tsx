fed9217f3fa5580f327ff04c2d138960fc3efc2b
Climax Mba <climaxium150@gmail.com>
2024-08-19 10:52:58 +0100

    Add debounce for performance

--- src/components/bookList/bookList.tsx
+++ src/components/bookList/bookList.tsx
@@ -1,6 +1,7 @@
-import { SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
+import useDebounce from "../../_lib/customHooks/useDebounce.ts";
 import BookItem from "../bookItem/BookItem.tsx";
 import Loading, { LoadingError } from "../loading/loading";
 import { paths } from "../../_lib/constants.ts";
@@ -16,93 +17,103 @@ export default function BookList({
   getBooks: () => Promise<Book[] | []>;
   hasBookId: boolean;
 }) {
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState<Book[] | []>([]);
+  const [error, setError] = useState(false);
+
+  const [params, setParams] = useSearchParams();
+  const [filteredBooks, setFilteredBooks] = useState(books);
+  const [searchQuery, setSearchQuery] = useState("");
+  const debouncedSearchQuery = useDebounce(searchQuery, 1000);
   const [filters, setFilters] = useState<{
     minPrice: number;
     maxPrice: number | null;
   }>({ minPrice: 0, maxPrice: null });
-  const [loading, setLoading] = useState(true);
-  const [books, setBooks] = useState<Book[] | []>([]);
-  const [error, setError] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [params, setParams] = useSearchParams();
-  const [filteredBooks, setFilteredBooks] = useState(books);
+
   const { q } = Object.fromEntries(params);
   const isInBooksPage = location.pathname.includes(paths.books);
 
+  // Load books
   useEffect(() => {
     (async () => {
       try {
-        setBooks((await getBooks()) as SetStateAction<Book[] | []>);
+        const bookArray = await getBooks();
+        if (!bookArray.length) throw new Error()
+        setBooks(bookArray);
       } catch {
         setError(true);
       }
-      setLoading(false);
     })();
   }, [getBooks]);
 
+  // Check search parameters
   useEffect(() => {
     if (isInBooksPage && q) setSearchQuery(q);
     setParams({});
   }, [isInBooksPage, q, setParams]);
 
+  // Set loading to true on `searchQuery` and `books` change
   useEffect(() => {
-    setFilteredBooks(
-      books.filter((book) => {
+    if (!error) setLoading(true);
+  }, [searchQuery, books, error]);
+
+  // Filter books
+  useEffect(() => {
+    const filteredArray = books.filter((book) => {
+      if (
+        (book.price >= filters.minPrice &&
+          filters.maxPrice !== null &&
+          book.price <= filters.maxPrice) ||
+        (book.price >= filters.minPrice && filters.maxPrice === null)
+      ) {
         if (
-          (book.price >= filters.minPrice &&
-            filters.maxPrice !== null &&
-            book.price <= filters.maxPrice) ||
-          (book.price >= filters.minPrice && filters.maxPrice === null)
-        ) {
-          if (
-            book.title.toLocaleLowerCase().includes(searchQuery.toLowerCase())
-          )
-            return true;
-        }
-        return false;
-      })
-    );
-  }, [books, filters, searchQuery]);
+          book.title
+            .toLocaleLowerCase()
+            .includes(debouncedSearchQuery.toLowerCase())
+        )
+          return true;
+      }
+      return false;
+    });
+    setFilteredBooks(filteredArray);
+    if (books.length) setLoading(false);
+  }, [books, filters, debouncedSearchQuery]);
 
   return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : error ? (
+    <div className={hasBookId ? styles.collapsableBooks : styles.books}>
+      <h1>Books</h1>
+      <SearchAndFilter
+        query={searchQuery}
+        setQuery={setSearchQuery}
+        filters={filters}
+        setFilters={setFilters}
+      />
+      {error ? (
         <LoadingError />
-      ) : (
-        <div className={hasBookId ? styles.collapsableBooks : styles.books}>
-          <h1>Books</h1>
-          <SearchAndFilter
-            query={searchQuery}
-            setQuery={setSearchQuery}
-            filters={filters}
-            setFilters={setFilters}
-          />
-          {filteredBooks.length ? (
-            <div className={styles.list}>
-              {filteredBooks.map((book) => (
-                <BookItem
-                  key={book.id}
-                  id={book.id}
-                  title={book.title}
-                  description={book.description}
-                  price={book.price}
-                  image={book.image}
-                  rating={book.rating}
-                  reviews={book.reviews}
-                  author={book.author}
-                  category={book.category}
-                  isTrending={book.isTrending}
-                  discount={book.discount}
-                />
-              ))}
-            </div>
-          ) : (
-            <p>No results!</p>
-          )}
+      ) : loading ? (
+        <Loading />
+      ) : filteredBooks.length ? (
+        <div className={styles.list}>
+          {filteredBooks.map((book) => (
+            <BookItem
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              description={book.description}
+              price={book.price}
+              image={book.image}
+              rating={book.rating}
+              reviews={book.reviews}
+              author={book.author}
+              category={book.category}
+              isTrending={book.isTrending}
+              discount={book.discount}
+            />
+          ))}
         </div>
+      ) : (
+        <p>No results!</p>
       )}
-    </>
+    </div>
   );
 }
--- src/components/categoryBookList/categoryBookList.tsx
+++ src/components/categoryBookList/categoryBookList.tsx
@@ -1,5 +1,6 @@
-import { SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
+import useDebounce from "../../_lib/customHooks/useDebounce.ts";
 import BookItem from "../bookItem/BookItem.tsx";
 import Loading, { LoadingError } from "../loading/loading.tsx";
 import SearchAndFilter from "../searchAndFilter/SearchAndFilter";
@@ -11,84 +12,92 @@ export default function CategoryBookList({
 }: {
   getBooks: () => Promise<Book[] | []>;
 }) {
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState<Book[] | []>([]);
+  const [error, setError] = useState(false);
+
+  const [filteredBooks, setFilteredBooks] = useState(books);
+  const [searchQuery, setSearchQuery] = useState("");
+  const debouncedSearchQuery = useDebounce(searchQuery, 1000);
   const [filters, setFilters] = useState<{
     minPrice: number;
     maxPrice: number | null;
   }>({ minPrice: 0, maxPrice: null });
-  const [loading, setLoading] = useState(true);
-  const [books, setBooks] = useState<Book[] | []>([]);
-  const [error, setError] = useState(false);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredBooks, setFilteredBooks] = useState(books);
 
+  // Load books
   useEffect(() => {
     (async () => {
       try {
-        setBooks((await getBooks()) as SetStateAction<Book[] | []>);
+        const bookArray = await getBooks();
+        if (!bookArray.length) throw new Error()
+        setBooks(bookArray);
       } catch {
         setError(true);
       }
-      setLoading(false);
     })();
   }, [getBooks]);
 
+  // Set loading to true on `searchQuery` and `books` change
   useEffect(() => {
-    setFilteredBooks(
-      books.filter((book) => {
+    if (!error) setLoading(true);
+  }, [searchQuery, books, error]);
+
+  // Filter books
+  useEffect(() => {
+    const filteredArray = books.filter((book) => {
+      if (
+        (book.price >= filters.minPrice &&
+          filters.maxPrice !== null &&
+          book.price <= filters.maxPrice) ||
+        (book.price >= filters.minPrice && filters.maxPrice === null)
+      ) {
         if (
-          (book.price >= filters.minPrice &&
-            filters.maxPrice !== null &&
-            book.price <= filters.maxPrice) ||
-          (book.price >= filters.minPrice && filters.maxPrice === null)
-        ) {
-          if (
-            book.title.toLocaleLowerCase().includes(searchQuery.toLowerCase())
-          )
-            return true;
-        }
-        return false;
-      })
-    );
-  }, [books, filters, searchQuery]);
+          book.title
+            .toLocaleLowerCase()
+            .includes(debouncedSearchQuery.toLowerCase())
+        )
+          return true;
+      }
+      return false;
+    });
+    setFilteredBooks(filteredArray);
+    if (books.length) setLoading(false);
+  }, [books, filters, debouncedSearchQuery]);
 
   return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : error ? (
+    <div className={styles.books}>
+      <SearchAndFilter
+        query={searchQuery}
+        setQuery={setSearchQuery}
+        filters={filters}
+        setFilters={setFilters}
+      />
+      {error ? (
         <LoadingError />
-      ) : (
-        <div className={styles.books}>
-          <SearchAndFilter
-            query={searchQuery}
-            setQuery={setSearchQuery}
-            filters={filters}
-            setFilters={setFilters}
-          />
-          {filteredBooks.length ? (
-            <div className={styles.list}>
-              {filteredBooks.map((book) => (
-                <BookItem
-                  key={book.id}
-                  id={book.id}
-                  title={book.title}
-                  description={book.description}
-                  price={book.price}
-                  image={book.image}
-                  rating={book.rating}
-                  reviews={book.reviews}
-                  author={book.author}
-                  category={book.category}
-                  isTrending={book.isTrending}
-                  discount={book.discount}
-                />
-              ))}
-            </div>
-          ) : (
-            <p>No results!</p>
-          )}
+      ) : loading ? (
+        <Loading />
+      ) : filteredBooks.length ? (
+        <div className={styles.list}>
+          {filteredBooks.map((book) => (
+            <BookItem
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              description={book.description}
+              price={book.price}
+              image={book.image}
+              rating={book.rating}
+              reviews={book.reviews}
+              author={book.author}
+              category={book.category}
+              isTrending={book.isTrending}
+              discount={book.discount}
+            />
+          ))}
         </div>
+      ) : (
+        <p>No results!</p>
       )}
-    </>
+    </div>
   );
 }
